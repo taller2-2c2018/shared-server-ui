d736442481da037b21f1febc5d0c4c219f6e1d89
nico <nicolas.angel.ledesma@gmail.com>
2018-04-18 23:43:20 -0300

    fixes en api.js y variables de entorno para conexion al backend

--- src/api/api.dist.js
+++
@@ -1,5 +0,0 @@
-const API_PORT = '3050'
-const API_VERSION = 'v0'
-const ROOT = 'localhost:' + API_PORT + '/' + API_VERSION + '/api/'
-const PROTOCOL = 'http'
-export const BASE = PROTOCOL + '://' + ROOT
--- src/api/api.js
+++ src/api/api.js
@@ -1,5 +1,5 @@
+const PROTOCOL = (process.env.REACT_APP_LOCAL==1)? 'http' : 'https'
 const API_PORT = '3050'
+const ROOT = (process.env.REACT_APP_LOCAL==1)? 'localhost:' + API_PORT : 'apinodebackend.herokuapp.com'
 const API_VERSION = 'v0'
-const ROOT = 'localhost:' + API_PORT + '/' + API_VERSION + '/api/'
-const PROTOCOL = 'http'
-export const BASE = PROTOCOL + '://' + ROOT
+export const BASE = PROTOCOL + '://' + ROOT + '/' + API_VERSION + '/api/'
